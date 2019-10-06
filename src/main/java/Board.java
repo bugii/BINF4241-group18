2f081c28f294516294d57ada20ca2064984bbce3
selemu <34353066+selemu@users.noreply.github.com>
2019-10-06 15:01:49 +0200

    Update Board.java

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Board {
 
@@ -10,7 +11,23 @@ public class Board {
         this.players = players;
         this.size = size;
     }
-    
+        private Snake MakeSnake(int n, Start f){
+        Snake s = new Snake(n, f);
+        int d = n - (new Random().nextInt(n-1) +1);
+        if (d < 1){ d = 1; } //technically shouldn't be needed, but still here for safety
+        Field desti = gameboard.get(d-1);
+        s.setDestination(desti);
+        return s;
+    }
+    private Ladder MakeLadder(int n, Start f){
+        Ladder l = new Ladder(n, f);
+        int d = n + (new Random().nextInt(size-n) +1);
+        if (d > size){ d=size; }
+        Field desti = gameboard.get(d-1);
+        l.setDestination(desti);
+        return l;
+    }
+
     public void MakeBoard () {
         if (gameboard.isEmpty()) {
             Start firstF = new Start(1, players);
@@ -21,7 +38,17 @@ public class Board {
             }
             Goal lastF = new Goal(size);
             gameboard.add(lastF);
-            //add ladders & snakes
+            //Now make Snakes and Ladders, every 5th field = Ladder, every 7th = Snake, Snakes have priority
+            for (int i = 2; i < size; i++) {
+                 if(i % 7 == 0){
+                     Snake s = MakeSnake(i, firstF);
+                     gameboard.set(i-1, s);
+                 }
+                 else if (i % 5 == 0){
+                     Ladder l = MakeLadder(i, firstF);
+                     gameboard.set(i-1, l);
+                 }
+            }
             for (Player p: players) {
                 p.setField(firstF);
             }
