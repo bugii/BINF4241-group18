4e4b428ee04810c8591658c0e47bf151de1e2dcd
selemu <34353066+selemu@users.noreply.github.com>
2019-10-06 12:50:23 +0200

    Update Board.java

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -10,26 +10,44 @@ public class Board {
         this.players = players;
         this.size = size;
     }
-
+    
     public void MakeBoard () {
-        Start firstF = new Start(1,players);
-        gameboard.add(firstF);
-        for (int i=2;i<size;i++) {
-            Field nextF = new Field(i, firstF);
-            gameboard.add(nextF);
+        if (gameboard.isEmpty()) {
+            Start firstF = new Start(1, players);
+            gameboard.add(firstF);
+            for (int i = 2; i < size; i++) {
+                Field nextF = new Field(i, firstF);
+                gameboard.add(nextF);
+            }
+            Goal lastF = new Goal(size);
+            gameboard.add(lastF);
+            //add ladders & snakes
+            for (Player p: players) {
+                p.setField(firstF);
+            }
         }
-        Goal lastF = new Goal(size);
-        gameboard.add(lastF);
-        //add ladders & snakes
+        else {
+            System.out.println("You have already created this board!");
+        }
     }
-
-    public void MoveFig (Player fig, int dist) {
-        //Moves Figure
+    
+    public void MoveFig (Player currPlayer, int distance) {
+        //moves player a certain distance on the board
+        Field currField = currPlayer.getField();
+        int desti = currField.getFieldNumber() + distance;
+        if (desti > size){
+            desti = size - (desti - size);
+        }
+        Field newF = gameboard.get(desti-1);
+        currField.getPlayers().remove(currPlayer);
+        newF.putOn(currPlayer);
     }
 
     public void PrintBoard () {
         for (Field f : gameboard){
+            System.out.print(" ");
             f.printField();
         }
+        System.out.println();
     }
 }
