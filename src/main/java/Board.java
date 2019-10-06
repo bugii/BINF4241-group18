a660b7d8c4727186e1921724167edca72ffaee82
selemu <34353066+selemu@users.noreply.github.com>
2019-10-06 20:44:04 +0200

    Update Board.java

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -10,8 +10,10 @@ public class Board {
     public Board (ArrayList<Player> players, int size) {
         this.players = players;
         this.size = size;
+        MakeBoard();
     }
-        private Snake MakeSnake(int n, Start f){
+    
+    private Snake MakeSnake(int n, Start f){
         Snake s = new Snake(n, f);
         int d = n - (new Random().nextInt(n-1) +1);
         if (d < 1){ d = 1; } //technically shouldn't be needed, but still here for safety
@@ -19,6 +21,7 @@ public class Board {
         s.setDestination(desti);
         return s;
     }
+    
     private Ladder MakeLadder(int n, Start f){
         Ladder l = new Ladder(n, f);
         int d = n + (new Random().nextInt(size-n) +1);
@@ -28,33 +31,28 @@ public class Board {
         return l;
     }
 
-    public void MakeBoard () {
-        if (gameboard.isEmpty()) {
-            Start firstF = new Start(1, players);
-            gameboard.add(firstF);
-            for (int i = 2; i < size; i++) {
-                Field nextF = new Field(i, firstF);
-                gameboard.add(nextF);
+    private void MakeBoard () {
+        Start firstF = new Start(1, players);
+        gameboard.add(firstF);
+        for (int i = 2; i < size; i++) {
+            Field nextF = new Field(i, firstF);
+            gameboard.add(nextF);
+        }
+        Goal lastF = new Goal(size);
+        gameboard.add(lastF);
+        //Now make Snakes and Ladders, every 5th field = Ladder, every 7th = Snake, Snakes have priority
+        for (int i = 2; i < size; i++) {
+            if(i % 7 == 0){
+                Snake s = MakeSnake(i, firstF);
+                gameboard.set(i-1, s);
             }
-            Goal lastF = new Goal(size);
-            gameboard.add(lastF);
-            //Now make Snakes and Ladders, every 5th field = Ladder, every 7th = Snake, Snakes have priority
-            for (int i = 2; i < size; i++) {
-                 if(i % 7 == 0){
-                     Snake s = MakeSnake(i, firstF);
-                     gameboard.set(i-1, s);
-                 }
-                 else if (i % 5 == 0){
-                     Ladder l = MakeLadder(i, firstF);
-                     gameboard.set(i-1, l);
-                 }
-            }
-            for (Player p: players) {
-                p.setField(firstF);
+            else if (i % 5 == 0){
+                Ladder l = MakeLadder(i, firstF);
+                gameboard.set(i-1, l);
             }
         }
-        else {
-            System.out.println("You have already created this board!");
+        for (Player p : players) {
+            p.setField(firstF);
         }
     }
     
